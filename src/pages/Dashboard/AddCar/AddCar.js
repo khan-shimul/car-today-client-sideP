f70eb53536da4b8921fd118ce51de6c9e95379ec
khan-shimul <khanshimul196@gmail.com>
2021-11-12 00:54:09 +0600

    send data to db

--- src/pages/Dashboard/AddCar/AddCar.js
+++ src/pages/Dashboard/AddCar/AddCar.js
@@ -1,10 +1,20 @@
+import axios from 'axios';
 import React from 'react';
 import { useForm } from "react-hook-form";
 import './AddCar.css';
 
 const AddCar = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+
+    // add cars api
+    const onSubmit = data => {
+        axios.post('http://localhost:5000/cars', { data })
+            .then(result => {
+                if (result.data.insertedId) {
+                    alert('added successfully')
+                }
+            })
+    };
 
     return (
         <div className="add-car-form m-5">
@@ -12,6 +22,7 @@ const AddCar = () => {
                 <h2 className="mb-3">Please add a car</h2>
                 <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column justify-content-center">
                     <input placeholder="Car Name" {...register("name", { required: true })} className="mb-3 p-3 border rounded-2" />
+                    <input type="number" placeholder="Speed" {...register("fast", { required: true })} className="mb-3 p-3 border rounded-2" />
                     <input type="number" placeholder="Price" {...register("price", { required: true })} className="mb-3 p-3 border rounded-2" />
                     <input type="number" placeholder="Rating" {...register("rating", { required: true, min: 0, max: 5 })} className="mb-3 p-3 border rounded-2" />
                     <input placeholder="Color" {...register("color", { required: true })} className="mb-3 p-3 border rounded-2" />
