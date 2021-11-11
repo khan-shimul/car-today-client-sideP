df7f3b86e385bdd8ce83796640649efc975932d5
khan-shimul <khanshimul196@gmail.com>
2021-11-11 16:49:20 +0600

    update header

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../../images/logo/logo.png';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
@@ -7,7 +8,15 @@ const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand as={NavLink} to="/">Car-Today</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src={logo}
+
+                        className="d-inline-block align-top"
+                    />{' '}
+
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
