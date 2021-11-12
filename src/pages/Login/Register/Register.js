7fb40c9512373cd681457f4f07cdd8b01abcca80
khan-shimul <khanshimul196@gmail.com>
2021-11-12 17:17:55 +0600

    set error message

--- src/pages/Login/Register/Register.js
+++ src/pages/Login/Register/Register.js
@@ -17,16 +17,16 @@ const Register = () => {
             setError('Password should be 6 character')
             return
         }
-        // pass validation for 1 special character
-        if (!/(?=.*[!@#$&*])/.test(data.password)) {
-            setError('Password should be one special character')
-            return
-        }
-        // check both pass same
-        if (data.password !== data.password2) {
-            alert('Password and confirm password should be match')
-            return
-        }
+        // // pass validation for 1 special character
+        // if (!/(?=.*[!@#$&*])/.test(data.password)) {
+        //     setError('Password should be one special character')
+        //     return
+        // }
+        // // check both pass same
+        // if (data.password !== data.password2) {
+        //     alert('Password and confirm password should be match')
+        //     return
+        // }
         // create new user
         registerNewUser(data.email, data.password)
         reset();
@@ -44,7 +44,7 @@ const Register = () => {
                         <input placeholder="Email" {...register("email", { required: true })} className="mb-2 p-3 border rounded-2" />
                         <input type="password" placeholder="Password" {...register("password", { required: true })} className="mb-2 p-3 border rounded-2" />
                         <input type="password" placeholder="Confirm Password" {...register("password2", { required: true })} className="mb-2 p-3 border rounded-2" />
-                        <p><small>{error}</small></p>
+                        <p><small className="text-danger">{error}</small></p>
                         <input type="submit" value="Register" className="btn btn-danger p-3" />
                         <p className="mt-2">Already have an account? <Link to="/login" className="text-decoration-none">Login</Link> </p>
                     </form>}
